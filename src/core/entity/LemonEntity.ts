2b2ee5ed8f522753ecdf7ef0d1bb2906e78844cc
liuyulvv <liuyulvv@outlook.com>
2025-08-11 19:50:47 +0800

    feat: root parent and children method

--- src/core/entity/LemonEntity.ts
+++ src/core/entity/LemonEntity.ts
@@ -4,14 +4,32 @@ import { v4 as uuidv4 } from "uuid";
 export default class LemonEntity extends Mesh {
   protected scene: Scene = this._scene;
 
-  protected showStatus: boolean = true;
-
   public constructor() {
     super(uuidv4());
   }
 
   public show(flag: boolean): void {
-    this.showStatus = flag;
-    this.setEnabled(flag);
+    this.isVisible = flag;
+  }
+
+  public getRootEntity(): LemonEntity {
+    let root = this as LemonEntity;
+    while (root && root.parent) {
+      root = root.parent as LemonEntity;
+    }
+    return root;
+  }
+
+  public getParentEntity(): LemonEntity | null {
+    let parent = this.parent as LemonEntity;
+    return parent || null;
+  }
+
+  public getChildrenEntity(): Array<LemonEntity> {
+    return this.getChildren().map((child) => child as LemonEntity);
+  }
+
+  public getAllChildrenEntity(): Array<LemonEntity> {
+    return this.getChildMeshes().map((child) => child as LemonEntity);
   }
 }
